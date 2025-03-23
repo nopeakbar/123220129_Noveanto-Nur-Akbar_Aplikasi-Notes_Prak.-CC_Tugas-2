1b1b66cc21dc089f28d831771135f23c25a03f2c
nopeakbar <114820984+nopeakbar@users.noreply.github.com>
2025-03-23 19:43:16 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,4 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(NoteRoute);
 
+// Tambahkan route default
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
 app.listen(8080, () => console.log("Server connected"));
