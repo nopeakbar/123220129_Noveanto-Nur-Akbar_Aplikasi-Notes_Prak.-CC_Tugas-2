07f672f02d4233949d5d63aefe65764995765dab
nopeakbar <114820984+nopeakbar@users.noreply.github.com>
2025-04-16 02:19:15 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import NoteRoute from "./routes/NoteRoute.js";
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(NoteRoute);
@@ -13,4 +14,9 @@ app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-app.listen(8080, () => console.log("Server connected"));
+// Gunakan PORT dari environment variable (Cloud Run menyetelnya secara otomatis)
+const port = process.env.PORT || 8080;  // Jika tidak ada PORT, default ke 8080
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
