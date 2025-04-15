a6e3854a792a5ddeb74f5cc437f6485c8ba52b90
nopeakbar <114820984+nopeakbar@users.noreply.github.com>
2025-04-16 01:02:53 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,11 @@ import NoteRoute from './routes/NoteRoute.js';
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(NoteRoute);
 
-// Route Default
+// Default route
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
@@ -19,7 +18,7 @@ app.get('/health', (req, res) => {
   res.status(200).send('Healthy');
 });
 
-// Listen on the PORT set by Cloud Run or fallback to 8080
+// Listen on PORT environment variable or fallback to 8080
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
