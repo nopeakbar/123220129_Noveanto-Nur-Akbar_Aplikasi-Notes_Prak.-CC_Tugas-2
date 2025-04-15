ee9bec67ffe4730f1f4e714d9ab8087b4132ee7a
nopeakbar <114820984+nopeakbar@users.noreply.github.com>
2025-04-16 02:15:29 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import NoteRoute from './routes/NoteRoute.js';
+import express from "express";
+import cors from "cors";
+import NoteRoute from "./routes/NoteRoute.js";
 
 const app = express();
 
@@ -8,8 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use(NoteRoute);
 
-// Mendengarkan port yang ditentukan oleh Cloud Run atau fallback ke 3000
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server berjalan di port ${port}`);
+// Tambahkan route default
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
 });
+
+app.listen(8080, () => console.log("Server connected"));
